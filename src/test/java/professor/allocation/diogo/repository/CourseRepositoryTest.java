63e41b598a6bab55fa67101ecdf3129207eb47a5
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-03-11 15:09:03 -0300

    add test crud

---
+++ src/test/java/professor/allocation/diogo/repository/CourseRepositoryTest.java
@@ -0,0 +1,5 @@
+package professor.allocation.diogo.repository;
+
+public class CourseRepositoryTest {
+
+}
---
+++ src/test/java/professor/allocation/diogo/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,82 @@
+package professor.allocation.diogo.repository;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import professor.allocation.diogo.entity.Department;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+public class DepartmentRepositoryTest {
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+	
+	@Test
+	public void findAll() {
+		List<Department> departments = departmentRepository.findAll();
+		
+		departments.forEach(System.out::println);
+	}
+	
+	@Test
+	public void findById() {
+		
+		Long id = 12L;
+		
+		Department department = departmentRepository.findById(id).orElse(null);
+		
+		System.out.println(department);
+		
+	}
+	
+	@Test
+	public void save_create() {
+		Department department = new Department();
+		department.setId(null);
+		department.setName("Department 6");
+		
+		department = departmentRepository.save(department);
+		
+		System.out.println(department);
+		
+	}
+	
+	@Test
+	public void save_update() throws ParseException {
+		Department department = new Department();
+		
+		department.setId(1L);
+		department.setName("Department upd");
+		
+		department = departmentRepository.save(department);
+		
+		System.out.println(department);
+	}
+	
+	@Test
+	public void deleteById() {
+		Long id = 12L;
+		
+		departmentRepository.deleteById(id);
+	}
+	
+	@Test
+	public void deleteAll() {
+		
+		departmentRepository.deleteAllInBatch();
+	}
+	
+	
+	
+}
