c623d2e09402976b6e6444b66f3c31972f44545d
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-04-14 23:04:10 -0300

    add tests CRUD AllocationService Junit

--- src/test/java/professor/allocation/diogo/service/AllocationServiceTest.java
+++ src/test/java/professor/allocation/diogo/service/AllocationServiceTest.java
@@ -31,6 +31,33 @@ public class AllocationServiceTest {
 	}
 	
 	@Test
+	public void findByProfessor() {
+		Long id = 1L;
+		
+		List<Allocation> allocations = allocationService.findByProfessor(id);
+		
+		allocations.forEach(System.out::println);
+	}
+	
+	@Test
+	public void findByCourse() {
+		Long id = 1L;
+		
+		List<Allocation> allocations = allocationService.findByCourse(id);
+		
+		allocations.forEach(System.out::println);
+	}
+	
+	@Test
+	public void findById() {
+		Long id = 1L;
+		
+		Allocation allocation = allocationService.findById(id);
+		
+		System.out.println(allocation);
+	}
+	
+	@Test
 	public void save() throws ParseException{
 		
 		Allocation allocation = new Allocation();
@@ -44,6 +71,34 @@ public class AllocationServiceTest {
 		allocation = allocationService.save(allocation);
 		
 		System.out.println(allocation);
+	}
+	
+	@Test
+	public void update() throws ParseException{
+		Allocation allocation = new Allocation();
+		allocation.setId(1L);
+		allocation.setDayOfWeek(DayOfWeek.MONDAY);
+		allocation.setStartHour(sdf.parse("19:00-0300"));
+		allocation.setEndHour(sdf.parse("20:00-0300"));
+		allocation.setProfessorId(1L);
+		allocation.setCourseId(1L);
+		
+		allocation = allocationService.update(allocation);
+		
+		System.out.println(allocation);
+		
+	}
+	
+	@Test
+	public void deleteById() {
+		Long id = 1L;
+		
+		allocationService.deleteById(id);
+	}
+	
+	@Test
+	public void deleteAll() {
+		allocationService.deleteAll();
 	}
 	
 	
