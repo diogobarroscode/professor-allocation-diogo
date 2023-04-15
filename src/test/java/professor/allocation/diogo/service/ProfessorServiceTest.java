3349f85307f76a40a398b630d3f07483ac87a978
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-04-14 23:49:53 -0300

    implement junit test CRUD for ProfessorService

--- src/test/java/professor/allocation/diogo/service/ProfessorServiceTest.java
+++ src/test/java/professor/allocation/diogo/service/ProfessorServiceTest.java
@@ -1,5 +1,98 @@
 package professor.allocation.diogo.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import professor.allocation.diogo.entity.Professor;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 public class ProfessorServiceTest {
 
+	@Autowired
+	ProfessorService professorService;
+	
+	@Test
+	public void findAll() {
+		List<Professor> professors = professorService.findAll(null);
+		
+		professors.forEach(System.out::println);
+	}
+	
+	@Test
+	public void findAllByName() {
+		String name = "professor";
+		
+		List<Professor> professors = professorService.findAll(name);
+		
+		professors.forEach(System.out::println);
+	}
+	
+	@Test
+	public void findByDepartment() {
+		Long departmentId = 1L;
+		
+		List<Professor> professors = professorService.findByDepartment(departmentId);
+		
+		professors.forEach(System.out::println);
+	}
+	
+	@Test
+	public void findById() {
+		Long id = 1L;
+		
+		Professor professor = professorService.findById(id);
+		
+		System.out.println(professor);
+	}
+	
+	@Test
+	public void save() {
+		Professor professor = new Professor();
+		professor.setId(null);
+		professor.setName("Professor 1");
+		professor.setCpf("111.111.111-11");
+		professor.setDepartmentId(1L);
+		
+		professor = professorService.save(professor);
+		
+		System.out.println(professor);
+	}
+	
+	@Test
+	public void update() {
+		Professor professor = new Professor();
+		professor.setId(1L);
+		professor.setName("Professor 2");
+		professor.setCpf("222.222.222-22");
+		professor.setDepartmentId(1L);
+		
+		professor = professorService.save(professor);
+		
+		System.out.println(professor);
+	}
+	
+	@Test
+	public void deleteById() {
+		Long id = 1L;
+		
+		professorService.deleteById(id);
+		
+		Professor professor = professorService.findById(id);
+		
+		System.out.println(professor);
+	}
+	
+	@Test
+	public void deleteAll() {
+		professorService.deleteAll();
+		
+		List<Professor> professors = professorService.findAll(null);
+		professors.forEach(System.out::println);
+	}
+	
 }
